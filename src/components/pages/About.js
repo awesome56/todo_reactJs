7f1e0502f0ff498415c998cdc197db98280b23de
Awe Oluwaseun <awesononeil@gmail.com>
2020-06-20 20:13:04 +0100

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Todo list app v1.0.0. It is my first React learning project.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
